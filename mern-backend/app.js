2a19185132411af6f611b80658fced980f20dfb1
nourbitbucket <mohamednjikam25@hotmail.com>
2021-04-01 20:23:48 +0300

    Set up the proj

--- mern-backend/app.js
+++ mern-backend/app.js
@@ -4,6 +4,8 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv/config");
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
 
 app.use(cors());
 app.options("*", cors());
@@ -11,6 +13,10 @@ app.options("*", cors());
 //middleware
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use(authJwt());
+app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use(errorHandler);
+
 
 //Routes
 const categoriesRoutes = require("./routes/categories");
